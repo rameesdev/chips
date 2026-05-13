5d411ddf9233b1f41c5cb64ac2967acfa5802a49
rameesdev <muhammadramees697@gmail.com>
2026-05-13 17:26:04 +0530

    vercel cong

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the root directory
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// Serve static files from the public directory
+app.use('/assets', express.static(path.join(__dirname, 'public', 'assets')));
 
 // Serve index.html for all routes
 app.get('*', (req, res) => {
